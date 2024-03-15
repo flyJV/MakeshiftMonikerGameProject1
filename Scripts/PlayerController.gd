a992c9dfa2519da7b6365a43edd2a83fa9e45331
Josias V <jvaldivia24@student.rccd.edu>
2024-03-14 20:24:38 -0700

    Movement Tweak 1

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -1,10 +1,11 @@
 extends KinematicBody2D
 
 # Declare member variables here.
-export var active = true 
+export var move_H_on = true
+export var move_V_on = true
 export var gravity_active = true 
 export var speed = 64
-export var fall_sofen = 0.83
+export var fall_sofen = 98
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,25 +18,32 @@ func _physics_process(_delta):
 	var move = Vector2(0, 0)
 	var fall = 0
 	
-	# 
+	# Gravity - just fall.
 	if (gravity_active):
-		fall += (fall * fall_sofen)
-	
-	# Respond to controler keys if active.
-	#  Keep physics running otherwise, but don't allow movement.
-	if (active):
+		if ((fall <= 0) && !is_on_floor()):
+			fall = 1
 		# Clear.
-		move.y = 0
 		move.x = 0
-		# Horizontal movement
+		# Calculate
+		fall += (fall * fall_sofen)
+		move.y = fall
+		# Drop.
+		move_and_slide(move)
+	# Clear.
+	move.y = 0
+	move.x = 0
+	# Horizontal movement
+	if (move_H_on):
 		if (Input.is_action_pressed("action_left")):
 			move.x += -1
 		if (Input.is_action_pressed("action_right")):
 			move.x = 1
-		# Vertical movement
+	# Vertical movement
+	if (move_V_on):
 		if (Input.is_action_pressed("action_up")):
 			move.y += -1
 		if (Input.is_action_pressed("action_down")):
 			move.y += 1
+	# Just move.
 	move_and_slide((move*speed))
 	pass
